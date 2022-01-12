bcf15c4f62bf5c562e591bc828afb17a0b6d41fe
Acher0ns <kamron.cole13@gmail.com>
2022-01-12 09:45:59 -0500

    start making tests

---
+++ tests/clone_repos_test.py
@@ -0,0 +1,6 @@
+import clone_repos
+
+DATE = '2022-01-12'
+TIME = '09:30'
+ASSIGNMENT_NAME = 'test'
+ORGANIZATION_NAME = 'GradingScriptsTest'
---
+++ tests/clone_script_test.py
@@ -0,0 +1,4 @@
+DATE = '2022-01-12'
+TIME = '09:30'
+ASSIGNMENT_NAME = 'test'
+ORGANIZATION_NAME = 'GradingScriptsTest'
