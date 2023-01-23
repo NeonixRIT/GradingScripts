585855e5b99666269712d1fadfed9ed0a53293ff
NeonixRIT <kamron.cole13@gmail.com>
2023-01-23 11:21:26 -0500

    continue update work

--- utils/update.py
+++ utils/update.py
@@ -1,14 +1,7 @@
-import os
 import subprocess
 import sys
 
 from pathlib import Path
-
-
-def spawn_program_and_die(program, exit_code=0):
-    subprocess.Popen(program)
-    sys.exit(exit_code)
-
 
 update_with_git = False
 update_with_download = False
@@ -21,10 +14,10 @@ else:
 
 if update_with_git:
     try:
-        subprocess.run('git fetch origin', cwd='../', stdout=subprocess.PIPE, stdin=subprocess.PIPE)
-        subprocess.run('git reset --hard origin/master', cwd='../', stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+        subprocess.run(['git', 'fetch', 'origin'], cwd='../', stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+        subprocess.run(['git', 'reset', '--hard', 'origin/master'], cwd='../', stdout=subprocess.PIPE, stdin=subprocess.PIPE)
     except Exception as e:
         print('Unable to complete auto update.')
         exit()
     print('Update was a success.')
-    spawn_program_and_die([sys.executable, '../GCISScripts.py'])
+    subprocess.run([sys.executable, '../GCISScripts.py'], cwd='../')
