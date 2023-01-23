406b969a53004706d49c3d33ad36279c547ade32
NeonixRIT <kamron.cole13@gmail.com>
2023-01-23 12:33:26 -0500

    test updating

--- tuiframeworkpy/model/utils.py
+++ tuiframeworkpy/model/utils.py
@@ -72,7 +72,9 @@ def print_updates(current_version: str):
     # input('Press enter to continue...')
     res = bool_prompt('Do you wish to update now?', True)
     if res:
-        update()
+        print(list(releases)[0].tag_name, os.getcwd())
+        input()
+        update(list(releases)[0].tag_name, os.getcwd())
 
 
 def make_new_config() -> SimpleNamespace:
