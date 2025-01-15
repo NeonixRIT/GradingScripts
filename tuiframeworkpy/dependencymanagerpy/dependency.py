b8c2b2338de19aa3bf91a578eaa2db0652ffd6ef
NeonixRIT <kamron.cole13@gmail.com>
2025-01-15 12:50:48 -0500

    optimize the dependency check and install methods to have more unified logic

--- tuiframeworkpy/dependencymanagerpy/dependency.py
+++ tuiframeworkpy/dependencymanagerpy/dependency.py
@@ -22,6 +22,8 @@ class IndependentDependencyError(Exception):
 
 
 CACHED_RESULTS = {}
+UV_CHECKED = False
+UV_INSTALLED = False
 
 class Dependency:
     def __init__(
@@ -41,20 +43,39 @@ class Dependency:
         self.up_to_date = False
 
     def install(self, upgrade: bool = False):
-        args = []
-
-        sub_command = 'install' if not upgrade else 'upgrade'
-        if self.package_manager_cmd == 'pip':
-            try:
-                subprocess.check_call(['uv', '--help'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-                args = ['uv', 'pip', 'install', self.package]
-            except FileNotFoundError:
-                args = [sys.executable, '-m', 'pip', 'install', self.package, '--upgrade']
-        else:
-            args = [self.package_manager_cmd, sub_command, self.package]
+        global UV_CHECKED, UV_INSTALLED
 
         if not self.package_manager_cmd:
             raise IndependentDependencyError(self)
+
+        # Decide sub_command for non-pip package managers
+        sub_command = 'install' if not upgrade else 'upgrade'
+
+        if self.package_manager_cmd == 'pip':
+            # If we haven't yet checked for `uv`, do it once
+            if not UV_CHECKED:
+                try:
+                    subprocess.check_call(
+                        ['uv', '--help'],
+                        stdout=subprocess.PIPE,
+                        stderr=subprocess.PIPE
+                    )
+                    UV_INSTALLED = True
+                except FileNotFoundError:
+                    UV_INSTALLED = False
+                UV_CHECKED = True
+
+            # Build the args for pip
+            if UV_INSTALLED:
+                args = ['uv', 'pip', 'install']
+            else:
+                args = [sys.executable, '-m', 'pip', 'install']
+            if upgrade:
+                args.append('--upgrade')
+            args.append(self.package)
+        else:
+            # For non-pip package managers, use the sub_command
+            args = [self.package_manager_cmd, sub_command, self.package]
 
         try:
             subprocess.check_call(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -62,46 +83,72 @@ class Dependency:
             raise DependencyInstallationError(self, args, cpe) from cpe
 
     def check(self) -> tuple[bool, bool]:
-        args = []
+        global UV_CHECKED, UV_INSTALLED
+
+        # Determine the command to run for version checking
         if self.package_manager_cmd == 'pip':
-            try:
-                subprocess.check_call(('uv', '--version'), stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+            # If we've never checked for 'uv' before, do so
+            if not UV_CHECKED:
+                try:
+                    subprocess.check_call(
+                        ('uv', '--version'), 
+                        stderr=subprocess.PIPE, 
+                        stdout=subprocess.PIPE
+                    )
+                    UV_INSTALLED = True
+                except FileNotFoundError:
+                    UV_INSTALLED = False
+                UV_CHECKED = True
+
+            # Now pick the freeze command
+            if UV_INSTALLED:
                 args = ('uv', 'pip', 'freeze', '--color=never')
-            except FileNotFoundError:
+            else:
                 args = (sys.executable, '-m', 'pip', 'freeze', '--no-color')
         else:
+            # For non-pip dependencies, just call '--version' (or version_flag)
             args = (self.package, self.version_flag)
 
         try:
-            current_version = None
+            # Check cache first
             if args in CACHED_RESULTS:
-                if self.package_manager_cmd == 'pip':
-                    current_version = re.findall(f'{self.package}=={self.version_regex}', CACHED_RESULTS[args])
-                else:
-                    current_version = re.findall(f'{self.version_regex}', CACHED_RESULTS[args])
+                result = CACHED_RESULTS[args]
             else:
                 result = subprocess.check_output(args).decode().strip()
                 CACHED_RESULTS[args] = result
-                if self.package_manager_cmd == 'pip':
-                    current_version = re.findall(f'{self.package}=={self.version_regex}', CACHED_RESULTS[args])
-                else:
-                    current_version = re.findall(f'{self.version_regex}', CACHED_RESULTS[args])
+
+            # Extract the current version from the result
+            if self.package_manager_cmd == 'pip':
+                pattern = f'{self.package}=={self.version_regex}'
+            else:
+                pattern = f'{self.version_regex}'
+            match = re.findall(pattern, result)
+            if not match:
+                # If we can’t find a matching version, treat it as not installed
+                raise IndexError
+
+            current_version = Version(match[0])
+
             if self.verbose:
                 print(f'    args: {args}')
                 print(f'    Current version: {current_version}')
-            current_version = Version(current_version[0])
+
+            # Compare the found version with the required version
             if current_version >= self.req_version:
                 self.installed = True
                 self.up_to_date = True
                 if self.verbose:
                     print(f'{self.package} is installed and up to date\n')
                 return self.installed, self.up_to_date
-            if self.verbose:
-                print(f'{self.package} is installed but out of date\n')
-            self.installed = True
-            self.up_to_date = False
-            return self.installed, self.up_to_date
+            else:
+                self.installed = True
+                self.up_to_date = False
+                if self.verbose:
+                    print(f'{self.package} is installed but out of date\n')
+                return self.installed, self.up_to_date
+
         except (subprocess.CalledProcessError, IndexError):
+            # Either the command failed or the version wasn’t in the output
             self.installed = False
             self.up_to_date = False
             if self.verbose:
